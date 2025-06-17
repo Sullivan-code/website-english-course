28319dcff0c39046899a8ccb6b4a076f802d35a9
Sullivan-code <ericsullivan532@gmail.com>
2025-06-17 08:58:50 -0300

    att

--- src/app/cursos/lesson1/page.tsx
+++ src/app/cursos/lesson1/page.tsx
@@ -88,7 +88,7 @@ export default function LessonFoodAndDrink() {
             <ul className="list-disc pl-6 text-gray-600 space-y-2 mb-6">
               <li>
                 <button 
-                  onClick={() => playAudio('Toeat')} 
+                  onClick={() => playAudio('toeat')} 
                   className="text-blue-600 font-bold cursor-pointer hover:text-blue-800 transition-colors"
                 >
                   to eat
@@ -96,7 +96,7 @@ export default function LessonFoodAndDrink() {
               </li>
               <li>
                 <button 
-                  onClick={() => playAudio('Todrink')} 
+                  onClick={() => playAudio('todrink')} 
                   className="text-blue-600 font-bold cursor-pointer hover:text-blue-800 transition-colors"
                 >
                   to drink
@@ -340,7 +340,7 @@ export default function LessonFoodAndDrink() {
             <ul className="list-disc pl-6 text-gray-600 space-y-2 mb-6">
               <li>
                 <button 
-                  onClick={() => playAudio('Ieatcrackersandyou')} 
+                  onClick={() => playAudio('icrackersandyou')} 
                   className="text-blue-600 font-bold cursor-pointer hover:text-blue-800 transition-colors"
                 >
                   I eat crackers. And you?
